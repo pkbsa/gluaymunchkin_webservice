75f61f4fef3fb514815cdad5dd353d1e4187c7bb
pkbsa <siranut.poko@hotmail.com>
2022-12-21 01:18:07 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+const cors = require("cors");
+var path = require("path");
+
+var app = express();
+
+app.use(
+    session({
+      secret: "secret",
+      resave: true,
+      saveUninitialized: true,
+    })
+);
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const publicDirectory = path.join(__dirname + "/css");
+app.use(express.static(publicDirectory));
+
+app.set("view engine", "ejs");
+
+app.get("/", function (request, response) {
+    response.render("index");
+});
+
+app.listen(3000, function(){
+    console.log("Listening at Port 3000")
+});
---
+++ css/index.js
@@ -0,0 +1,20 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      console.log("TOP" + elementTop)
+      console.log("height" + windowHeight)
+      console.log("visible" + elementVisible)
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
